ca853a3ffaa9327d4f2690975f4f2fe73785dce7
piegames <git@piegames.de>
2025-11-15 20:37:31 +0100

    fix and improve default.nix

--- default.nix
+++ default.nix
@@ -1,38 +1,44 @@
+# Usage:
+# nix-build -A reports-combined \
+#     --arg nixA '{ url = "file://path/to/lix"; rev = "0000000000000000000000000000000000000000";}' \
+#     --arg nixB '{ url = "file://path/to/lix"; ref = "2.94.0";}'
+# or
+# nix-build -A reports.\"https://github.com/nixos/nixpkgs\" \
+#     --arg nixA /path/to/lix/repo \
+#     --arg nixB '{ url = "file://path/to/lix"; ref = "2.94.0";}'
 let
   pins = import ./npins;
   pkgs = import pins.nixpkgs { };
   lib = pkgs.lib;
-  # TODO
-  nixA = null;
-  nixB = null;
 in
-assert nixA != null;
-assert nixB != null;
 rec {
   flaker = pkgs.callPackage ./flaker.nix { };
 
   # Call all pins with a Nixpkgs to make them proper derivations
   sources = lib.mapAttrs (_: pin: pin { inherit pkgs; }) (import ./npins { input = ./test.json; });
 
-  reports = lib.mapAttrs (
-    name: pin:
-    pkgs.stdenvNoCC.mkDerivation {
-      inherit name;
-      src = pin.outPath;
-      buildInputs = [
-        flaker
-        nixA
-        nixB
-      ];
-      buildPhase = ''
-        flaker nix-parse . ${nixA}/bin/nix ${nixB}/bin/nix
-      '';
-      installPhase = "cp report.json $out";
-    }
-  ) sources;
+  reports =
+    { nixA, nixB }:
+    let
+      lixA = (import (builtins.fetchGit nixA)).default;
+      lixB = (import (builtins.fetchGit nixB)).default;
+    in
+    lib.mapAttrs (
+      name: pin:
+      pkgs.stdenvNoCC.mkDerivation {
+        inherit name;
+        src = pin.outPath;
+        buildInputs = [
+          flaker
+          lixA
+          lixB
+        ];
+        buildPhase = ''
+          flaker nix-parse . ${lixA}/bin/nix ${lixB}/bin/nix
+        '';
+        installPhase = "cp report.json $out";
+      }
+    ) sources;
 
-  reports-combined = pkgs.linkFarm "report-combined" {
-    # ./pin1, ./pin2, ...
-    paths = reports;
-  };
+  reports-combined = { nixA, nixB }@nixArgs: pkgs.linkFarm "report-combined" (reports nixArgs);
 }
